ea0730474aa27bf40cfd715d01a087f161e9b4a3
Sreerag <sreeragm46@gmail.com>
2020-04-24 10:25:31 +0530

    committed

---
+++ app/src/main/java/com/example/theerthamovers/Admin_module/Home_activity.java
@@ -0,0 +1,18 @@
+package com.example.theerthamovers.Admin_module;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.theerthamovers.R;
+
+public class Home_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_activity);
+
+
+    }
+}
